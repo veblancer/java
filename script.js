c089e1a391bcfb24afa02184fc651e2773be1d39
Umar bashir <veblancer1@gmail.com>
2026-01-13 00:30:53 +0530

    script

---
+++ script.js
@@ -0,0 +1,22 @@
+function tyre() {
+ const rung = document.getElementById("input").value; 
+ document.getElementById("outputtext").innerHTML = rung;
+ 
+}
+function aire() {
+ const red1 = document.getElementById("red"); 
+ red1.style.backgroundColor= "red";
+}
+ function wire() {
+ const blue1 = document.getElementById("blue"); 
+ blue1.style.backgroundColor= "blue";
+ }
+function bire() {
+ const green1 = document.getElementById("green"); 
+ green1.style.backgroundColor= "green";
+}
+ function cire() {
+ const yellow1 = document.getElementById("yellow"); 
+ yellow1.style.backgroundColor= "yellow";
+}
+ 
